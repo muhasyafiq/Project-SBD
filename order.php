0a5cdcbe72c3b092f0a2a4bc80bce5459c373a3a
muhasyafiq <111621066+muhasyafiq@users.noreply.github.com>
2023-11-29 16:39:20 +0700

    Order page

--- order.php
+++ order.php
@@ -7,30 +7,50 @@ if(!isset($_SESSION["user_id"])){
   exit;
 }
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+	<link rel="stylesheet" href="css/login.style.css">
     <title>Order</title>
-    <link rel="stylesheet" href="css/reset.css" />
-    <link rel="stylesheet" href="css/styles.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100&display=swap" rel="stylesheet">
 </head>
 <body>
-	<div class="login-box">
-    <h1>Order</h1>
-    <form action="includes/order.inc.php" method="post">
-      <input type="text" name="username" placeholder="Username">
-      <input type="text" name="package_name" placeholder="Package Name">
-      <input type="text" name="package_type" placeholder="Package Type">
-			<input type="text" name="amount" placeholder="Amount">
-			<input type="text" name="note" placeholder="Note">
-      <input type="submit" value="Place order" />
-    </form>
+	<div class="container">
+		<div class="sign-in-wrapper">
+			<form action="includes/order.inc.php" method="post">
+				<h1>Order</h1>
+				    <div class="input-group">
+              <input type="text" name="username" placeholder="Username">
+              <input type="text" name="package_name" placeholder="Package Name">
+              <input type="text" name="package_type" placeholder="Package Type">
+			        <input type="text" name="amount" placeholder="Amount">
+			        <input type="text" name="note" placeholder="Note">
+					    <button>Place Order</button>
+				    </div>
+				</form>
+				<?php
+   					check_login_errors();
+    			?>
+			</div>
+			<div class="overlay">
+					<div class="overlay-left">
+						<h1>Welcome Back</h1>
+						<p>To keep connected with us please login with your personal info.</p>
+						<button id="signin" class="overlay-btn">Sign in</button>
+					</div>
+					<div class="overlay-right">
+						<h1>Hello, Friend</h1>
+						<p>Enter your personal details and start journey with us.</p>
+						<button id="signup" class="overlay-btn">Sign up</button>
+					</div>
+			</div>
 	</div>
+
+	<script src="js/main.js"></script>
 </body>
 </html>
